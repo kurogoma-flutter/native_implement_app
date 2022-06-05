103fbb3f1a6200737a1937af456074d40d4d91eb
kurogoma939 <kan.sss.939@gmail.com>
2022-06-05 12:33:18 +0900

    swift側の実装

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
@@ -8,6 +9,20 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+    let helloMethodChannel = FlutterMethodChannel(name: "appInfo",binaryMessenger: controller as! FlutterBinaryMessenger)
+    helloMethodChannel.setMethodCallHandler({
+      (call:FlutterMethodCall,result:FlutterResult) -> Void in
+
+      switch call.method{
+          case "getAppVersion":
+              result(Bundle.main.object(forInfoDictionaryKey:"CFBundleShortVersionString") as? String)
+          default:
+             result("iOS" + UIDevice.current.systemVersion)
+      }
+      
+    })
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
